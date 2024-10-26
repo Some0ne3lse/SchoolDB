4da5ddc2def8cb1520f4dc8ca228719bb1659834
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-10-26 15:43:02 +0000

    (Hopefully) finish context

--- SchoolDB/Data/SchoolDBContext.cs
+++ SchoolDB/Data/SchoolDBContext.cs
@@ -14,6 +14,50 @@ public class SchoolDbContext : DbContext
     public DbSet<Subject> Subjects { get; set; }
 
     public DbSet<Teacher> Teachers { get; set; }
+
+    public static void SeedDataBase()
+    {
+        using var db = new SchoolDbContext();
+        db.Database.EnsureCreated();
+
+        var g1Exists = db.Groups.FirstOrDefault(g => g.Name == "GroupTest");
+        if (g1Exists == null)
+        {
+            Group g1 = new Group() { Name = "GroupTest" };
+            db.Groups.Add(g1);
+        }
+
+        var st1Exists = db.Students.FirstOrDefault(st => st.FirstName == "Testi" && st.LastName == "Test" && st.GroupId == g1Exists.Id);
+        if (st1Exists == null)
+        {
+            Student st1 = new Student() { FirstName = "Testi", LastName = "Test", GroupId = g1Exists.Id};
+            db.Students.Add(st1);
+        }
+
+        var su1Exists = db.Subjects.FirstOrDefault(su => su.Title == "SubjectTest");
+        if (su1Exists == null)
+        {
+            Subject su1 = new Subject() { Title = "SubjectTest" };
+            db.Subjects.Add(su1);
+        }
+
+        var t1Exists = db.Teachers.FirstOrDefault(t => t.FirstName == "Teacher" && t.LastName == "Test");
+        if (t1Exists == null)
+        {
+            Teacher t1 = new Teacher() { FirstName = "Teacher", LastName = "Test" };
+            db.Teachers.Add(t1);
+        }
+
+        var m1Exists = db.Marks.FirstOrDefault(m => m.StudentId == st1Exists.Id && m.SubjectId == su1Exists.Id && m.MarkReceived == 90);
+        if (m1Exists == null)
+        {
+            Mark m1 = new Mark() { Date = DateTime.Now, StudentId = st1Exists.Id, SubjectId = su1Exists.Id, MarkReceived = 90 };
+            db.Marks.Add(m1);
+
+        }
+        db.SaveChanges();
+
+    }
     
     public string DbPath { get; }
 
